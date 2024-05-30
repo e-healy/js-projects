521df5b50a469e34681ee5a92afcf6ac36abfb86
Ellie H <elliehealy20@outlook.com>
2024-05-30 12:42:11 +0100

    Add btn and set up event listener for click, create function for getting random color numbers.

--- Color-flipper/setup/app.js
+++ Color-flipper/setup/app.js
@@ -1,1 +1,18 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+// call elements
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+// set upevent listener for click events and add callback function, then set up our logic in the middle
+btn.addEventListener('click', function() {
+    // get random number between 0 - 3
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber)
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
