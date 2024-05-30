6d32693465c4c4ee3019ec2c2bc63e6235d9f78e
Ellie H <elliehealy20@outlook.com>
2024-05-30 12:58:08 +0100

    Create event listener for hex button. Create function using math.random to generate hex number

--- Color-flipper/setup/hex.js
+++ Color-flipper/setup/hex.js
@@ -1,1 +1,22 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+// add event listener
+btn.addEventListener('click', function() {
+    let hexColor = '#'
+    for(let i = 0; i<6; i++) {
+        hexColor += hex[getRandomNumber()]
+    }
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+});
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * hex.length);
+};
+
+
+// hex color consist of hashtag and 6 values and these values could be from 0-9, letters go from 10-15
+// to get hex value, we must have a hashtag, which is why we've created a variable hexColor
+//now we need a loop that goes around 6 times to create our hex color. 
