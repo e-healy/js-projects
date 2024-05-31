e13f6b0fce27055a2c640dbecf5cf96611b859c4
Ellie H <elliehealy20@outlook.com>
2024-05-31 13:19:40 +0100

    Add javascript variables and event listeners to buttons

--- Reviews/setup/app.js
+++ Reviews/setup/app.js
@@ -29,56 +29,56 @@ const reviews = [
     text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
   },
 ];
+
 // select items
-const img = document.getElementById('person-img');
-const author = document.getElementById('author');
-const job = document.getElementById('job');
-const info = document.getElementById('info');
+const img = document.getElementById('person-img')
+const author = document.getElementById('author')
+const job = document.getElementById('job')
+const info = document.getElementById('info')
 
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
-const randomBtn = document.querySelector('.random-btn');
+const prevBtn = document.querySelector('.prev-btn')
+const nextBtn = document.querySelector('.next-btn')
+const randomBtn = document.querySelector('.random-btn')
 
-// set starting item
+// set starting item of 0 
 let currentItem = 0;
 
-// load initial item
-window.addEventListener('DOMContentLoaded', function () {
-  const item = reviews[currentItem];
-  img.src = item.img;
-  author.textContent = item.name;
-  job.textContent = item.job;
-  info.textContent = item.text;
+// load intital item - domcontentloaded - 
+window.addEventListener('DOMContentLoaded', function() {
+  showPerson(currentItem);
 });
 
-// show person based on item
+// show person based on item - function to change item number
+
 function showPerson(person) {
   const item = reviews[person];
   img.src = item.img;
   author.textContent = item.name;
   job.textContent = item.job;
   info.textContent = item.text;
-}
-// show next person
-nextBtn.addEventListener('click', function () {
+};
+
+// show next person - set up event listener
+nextBtn.addEventListener('click', function() {
   currentItem++;
-  if (currentItem > reviews.length - 1) {
+  if(currentItem > reviews.length - 1) {
     currentItem = 0;
   }
   showPerson(currentItem);
 });
-// show prev person
-prevBtn.addEventListener('click', function () {
+
+// show previous person
+prevBtn.addEventListener('click', function() {
   currentItem--;
-  if (currentItem < 0) {
+  if(currentItem < 0) {
     currentItem = reviews.length - 1;
   }
   showPerson(currentItem);
 });
-// show random person
-randomBtn.addEventListener('click', function () {
-  console.log('hello');
 
+// surprise button
+randomBtn.addEventListener('click', function() {
   currentItem = Math.floor(Math.random() * reviews.length);
+  console.log(currentItem);
   showPerson(currentItem);
 });
